095e443a52abfdb5be9262f010f9ddc3c92b2d14
Hassan <meyian@gmail.com>
2017-12-08 04:30:09 +0000

    Update dilla.bundle.js

--- dilla.bundle.js
+++ dilla.bundle.js
@@ -1,3 +1,5 @@
+var dilla;
+
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var memoize = require('meemo');
 
@@ -164,7 +166,7 @@ module.exports = expressions;
 },{"meemo":10}],2:[function(require,module,exports){
 var Dilla = require('./index');
 var audioContext = new (window.AudioContext || window.webkitAudioContext)();
-var dilla = new Dilla(audioContext);
+dilla = new Dilla(audioContext);
 
 },{"./index":3}],3:[function(require,module,exports){
 var events = require('events');
